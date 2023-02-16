407773149f78930ca01ef927b4b84a44a37524ee
Sourcery AI <>
2023-02-16 16:04:27 +0000

    'Refactored by Sourcery'

--- ai.py
+++ ai.py
@@ -57,8 +57,8 @@ def take_user_input():
     try:
         print('Recognizing...')
         query = r.recognize_google(audio)
-        print("You said : {}".format(text))
-        if not 'exit' in query or 'stop' in query:
+        print(f"You said : {text}")
+        if 'exit' not in query or 'stop' in query:
             speak(choice(opening_text))
         else:
             hour = datetime.now().hour
@@ -80,24 +80,18 @@ if __name__ == '__main__':
         # open notepad
         if 'notepad' in query:
             open_notepad()
-        # open discord
         elif 'open discord' in query:
             open_discord()
-        # open cmd
         elif 'open command prompt' in query or 'open cmd' in query:
             open_cmd()
-        # open camera
         elif 'open camera' in query:
             open_camera()
-        # open calculator
         elif 'open calculator' in query:
             open_calculator()
-        # find my ip
         elif 'ip address' in query:
             ip_address = find_my_ip()
             speak(f'Your IP Address is {ip_address}.\n For your convenience, I am printing it on the screen oniisan.')
             print(f'Your IP Address is {ip_address}')
-        # wikipedia search
         elif 'wikipedia' in query:
             speak('What do you want to search on Wikipedia, oniisan?')
             search_query = take_user_input().lower()
@@ -105,17 +99,14 @@ if __name__ == '__main__':
             speak(f"According to Wikipedia, {results}")
             speak("For your convenience, I am printing it on the screen oniisan.")
             print(results)
-        # open youtube
         elif 'youtube' in query:
             speak('What do you want to play on Youtube, oniisan?')
             video = take_user_input().lower()
             play_on_youtube(video)
-        # google seacrh
         elif 'google' in query:
             speak('What do you want to search on Google, oniisan?')
             query = take_user_input().lower()
             search_on_google(query)
-        # send whatsapp message
         elif "send whatsapp message" in query:
             speak(
                 'On what number should I send the message oniisan? Please enter in the console: ')
@@ -124,7 +115,6 @@ if __name__ == '__main__':
             message = take_user_input().lower()
             send_whatsapp_message(number, message)
             speak("I've sent the message oniisan.")
-        # send email
         elif "email" in query:
             speak("On what email address do I send oniisan? Please enter in the console: ")
             receiver_address = input("Enter email address: ")
@@ -136,32 +126,27 @@ if __name__ == '__main__':
                 speak("I've sent the email oniisan.")
             else:
                 speak("Something went wrong while I was sending the mail. Please check the error logs oniisan.")
-        # gimme a joke XD
         elif 'joke' in query:
-            speak(f"Hope you like this one oniisan")
+            speak("Hope you like this one oniisan")
             joke = get_random_joke()
             speak(joke)
             speak("For your convenience, I am printing it on the screen oniisan.")
             pprint(joke)
-        # gimme a advice
         elif "advice" in query:
-            speak(f"Here's an advice for you, oniisan")
+            speak("Here's an advice for you, oniisan")
             advice = get_random_advice()
             speak(advice)
             speak("For your convenience, I am printing it on the screen oniisan.")
             pprint(advice)
-        # get a trending movies
         elif "trending movies" in query:
             speak(f"Some of the trending movies are: {get_trending_movies()}")
             speak("For your convenience, I am printing it on the screen oniisan.")
             print(*get_trending_movies(), sep='\n')
-        # get a trending news
         elif 'news' in query:
-            speak(f"I'm reading out the latest news headlines, oniisan")
+            speak("I'm reading out the latest news headlines, oniisan")
             speak(get_latest_news())
             speak("For your convenience, I am printing it on the screen oniisan.")
             print(*get_latest_news(), sep='\n')
-        # get weather today
         elif 'weather' in query:
             ip_address = find_my_ip()
             city = requests.get(f"https://ipapi.co/{ip_address}/city/").text
